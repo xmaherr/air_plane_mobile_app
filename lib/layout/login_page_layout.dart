a3a265e0c2e4214cb6ab71c849fe983442655604
omarebid1 <omar3bid18@gmail.com>
2024-04-03 12:13:59 +0200

    updated title & created new branch

--- lib/layout/login_page_layout.dart
+++ lib/layout/login_page_layout.dart
@@ -56,7 +56,7 @@ class _LoginPageLayoutState extends State<LoginPageLayout> {
                       ),
                     ),
                     Text(
-                      'Welcome to MARO Airlines',
+                      'Welcome to Omar Airlines',
                       style: TextStyle(
                         fontSize: 20,
                         color: Colors.white.withOpacity(0.8),
